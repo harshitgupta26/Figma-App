98c8736087d4fe24acca1fe72ec2ca043fa2faac
Harshit Gupta <26harshitgupta99@gmail.com>
2020-08-13 23:38:29 +0530

    heroku

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-include_once("./views/index.html");
+foreach(glob("views/*.html") as $filename) {
+    include_once($filename);
+}
+foreach(glob("scripts/*.js") as $filename) {
+    include_once($filename);
+}
+foreach(glob("vendor/*.css") as $filename) {
+    include_once($filename);
+}
+foreach(glob("vendor/*.js") as $filename) {
+    include_once($filename);
+}
+foreach(glob("styles/*.css") as $filename) {
+    include_once($filename);
+}
+
 
 ?>
